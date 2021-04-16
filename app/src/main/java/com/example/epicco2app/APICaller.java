1ad2f675b7d1b484e265f613c497e6d6051f45f6
audioAXS <akseli.suutari@hotmail.com>
2021-04-16 20:00:50 +0300

    Apikutsujen kehittelyä 2

--- app/src/main/java/com/example/epicco2app/APICaller.java
+++ app/src/main/java/com/example/epicco2app/APICaller.java
@@ -1,23 +1,45 @@
 package com.example.epicco2app;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class APICaller {
 
     private static APICaller caller = new APICaller();
-    ReturnData returnData = null;
+    private RequestQueue requestQueue;
 
-    private APICaller(){
+
+
+    APICaller(){
     }
+
+
     public static APICaller getInstance(){
         return caller;
 
     }
 
+    public void call(Context ctx) {
+        RequestQueue queue = Volley.newRequestQueue(ctx);
+        String url = "https://ilmastodieetti.ymparisto.fi/ilmastodieetti/calculatorapi/v1/FoodCalculator?query.diet=omnivore&query.beefLevel=10";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> System.out.println("Response: "+ response.toString()),
+                error -> System.out.println("Error"));
 
 
-
-    private static class ReturnData {
+        queue.add(request);
 
     }
 

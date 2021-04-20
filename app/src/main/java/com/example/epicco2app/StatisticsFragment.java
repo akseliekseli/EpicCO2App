13cce1f002e84cf5948dc88983a8261307c36f1f
emmus <emma.hirvonen@student.lut.fi>
2021-04-20 19:08:09 +0300

    Lisätty linechart painon seuraamiselle sekä kuukaudet x-akselille barcharttiin.

--- app/src/main/java/com/example/epicco2app/StatisticsFragment.java
+++ app/src/main/java/com/example/epicco2app/StatisticsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,67 +12,93 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class StatisticsFragment extends Fragment {
     BarChart barChart;
-    ArrayList<String> monthList;
+    LineChart lineChart;
     ArrayList<BarEntry> barEntriesMonths;
-    ArrayList<String> weekList; // Vai halutaanko päivittäinen enemmin
-    ArrayList<BarEntry> barEntriesWeeks;
+    RadioGroup selectTime;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.fragment_statistics, container, false);
         barChart = (BarChart) layout.findViewById(R.id.chart1);
+        lineChart =(LineChart) layout.findViewById(R.id.chart2);
+        selectTime = (RadioGroup) layout.findViewById(R.id.TimeGroup);
 
         // Testattu lisämällä nyt vaan käsin arvoja listaa
         barEntriesMonths = new ArrayList<>();
-        barEntriesMonths.add(new BarEntry(1, 80));
-        barEntriesMonths.add(new BarEntry(2, 200));
-        barEntriesMonths.add(new BarEntry(3, 40));
-        barEntriesMonths.add(new BarEntry(4, 150));
-        barEntriesMonths.add(new BarEntry(5, 45));
-        barEntriesMonths.add(new BarEntry(6, 50));
+        /*barEntriesMonths.add(new BarEntry(1, 80f));
+        barEntriesMonths.add(new BarEntry(2, 200f));
+        barEntriesMonths.add(new BarEntry(3, 40f));
+        barEntriesMonths.add(new BarEntry(4, 150f));
+        barEntriesMonths.add(new BarEntry(5, 45f));
+        barEntriesMonths.add(new BarEntry(6, 50f));*/
+
+        barEntriesMonths.add(new BarEntry(80f,0));
+        barEntriesMonths.add(new BarEntry(200f,1));
+        barEntriesMonths.add(new BarEntry(150f,2));
+        barEntriesMonths.add(new BarEntry(45f,3));
+        barEntriesMonths.add(new BarEntry(50f,4));
+        barEntriesMonths.add(new BarEntry(80f,5));
+        barEntriesMonths.add(new BarEntry(200f,6));
+        barEntriesMonths.add(new BarEntry(150f,7));
+        barEntriesMonths.add(new BarEntry(45f,8));
+        barEntriesMonths.add(new BarEntry(50f,9));
+        barEntriesMonths.add(new BarEntry(150f,10));
+        barEntriesMonths.add(new BarEntry(45f,11));
+
+
+        ArrayList<String> monthList = new ArrayList<>();
+        monthList.add("Tam");
+        monthList.add("Hel");
+        monthList.add("Maa");
+        monthList.add("Huh");
+        monthList.add("Tou");
+        monthList.add("Kes");
+        monthList.add("Hei");
+        monthList.add("Elo");
+        monthList.add("Syy");
+        monthList.add("Lok");
+        monthList.add("Mar");
+        monthList.add("Jou");
+
+
         BarDataSet barDataSet = new BarDataSet(barEntriesMonths, "Co2 kulutus");
-        // Näillä ei nyt josatin syystä toimi selvitän tätä vielä
-        /*ArrayList<String> monthList = new ArrayList<>();
-        monthList.add("Tammikuu");
-        monthList.add("Helmikuu");
-        monthList.add("Maaliskuu");
-        monthList.add("Huhtikuu");
-        monthList.add("Toukokuu");
-        monthList.add("Kesäkuu");
-        monthList.add("Heinäkuu");
-        monthList.add("Elokuu");
-        monthList.add("Syyskuu");
-        monthList.add("Lokakuu");
-        monthList.add("Marraskuu");
-        monthList.add("Joulukuu");*/
-
-        BarData theData = new BarData(barDataSet);
-
-        // Testattu lisämällä nyt vaan käsin arvoja listaa
-        /*barEntriesWeeks = new ArrayList<>();
-        barEntriesWeeks.add(new BarEntry(1, 45));
-        barEntriesWeeks.add(new BarEntry(2, 60));
-        barEntriesWeeks.add(new BarEntry(3, 40));
-        barEntriesWeeks.add(new BarEntry(4, 20));
-        barEntriesWeeks.add(new BarEntry(5, 35));
-        barEntriesWeeks.add(new BarEntry(6, 50));
-        BarDataSet barDataSet2 = new BarDataSet(barEntriesWeeks, "Co2 kulutus");*/
-
+        BarData theData = new BarData(monthList,barDataSet);
+        //BarData theData = new BarData(monthList,barDataSet);
         barChart.setData(theData);
-
+        //barChart.setDescription("");
+        //barChart.setBackgroundColor();
         barChart.setTouchEnabled(true);
         barChart.setDragEnabled(true);
         barChart.setScaleEnabled(true);
 
+        ArrayList<Entry> barEntries2 = new ArrayList<>();
+        barEntries2.add(new Entry(56, 1));
+        barEntries2.add(new Entry(57, 2));
+        barEntries2.add(new Entry(54, 3));
+        barEntries2.add(new Entry(59, 4));
+        barEntries2.add(new Entry(63, 5));
+        barEntries2.add(new Entry(60,6 ));
+        LineDataSet lineDataSet = new LineDataSet(barEntries2,"Paino");
+        LineData lineData = new LineData(monthList, lineDataSet);
+        lineChart.setData(lineData);
+
         return layout;
     }
+
 }

c59613c4e430d9689fec24d70b32b74b032ac66b
OttoSalmela <otto.salmela@outlook.com>
2021-04-20 10:10:37 +0300

    Added log out functionality.

--- app/src/main/java/com/example/epicco2app/HomeActivity.java
+++ app/src/main/java/com/example/epicco2app/HomeActivity.java
@@ -8,11 +8,13 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
@@ -61,7 +63,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                         new SettingsFragment()).commit();
                 break;
             case R.id.nav_logout:
-                Toast.makeText(this, "Kirjaudu ulos", Toast.LENGTH_SHORT).show();
+                FirebaseAuth.getInstance().signOut();
+                Intent toMain = new Intent(HomeActivity.this , LoginActivity.class);
+                startActivity(toMain);
                 break;
         }
         drawer.closeDrawer(GravityCompat.START);

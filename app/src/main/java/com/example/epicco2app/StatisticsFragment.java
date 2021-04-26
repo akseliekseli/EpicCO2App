adfd45b80878ba742aceba21f6a97e2fd41a2ff1
emmus <emma.hirvonen@student.lut.fi>
2021-04-26 15:18:46 +0300

    Lisätty kuvaaja viikko tiedoille, varsinainen toiminnnallisuus siis puuttuu

--- app/src/main/java/com/example/epicco2app/StatisticsFragment.java
+++ app/src/main/java/com/example/epicco2app/StatisticsFragment.java
@@ -31,13 +31,13 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class StatisticsFragment extends Fragment {
-    BarChart barChart;
+    BarChart barChart ,barChart2;
     LineChart lineChart;
     PieChart pieChart;
     ArrayList<BarEntry> barEntriesMonths;
-
     RadioGroup selectTime;
     IODatabase io;
     String userID;
@@ -45,20 +45,18 @@ public class StatisticsFragment extends Fragment {
 
     @Nullable
     @Override
-
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_statistics, container,false);
-        barChart = (BarChart) layout.findViewById(R.id.chart1);
-        lineChart =(LineChart) layout.findViewById(R.id.chart2);
-        pieChart = (PieChart) layout.findViewById(R.id.chart3);
-        selectTime = (RadioGroup) layout.findViewById(R.id.TimeGroup);
+        barChart = (BarChart) layout.findViewById(R.id.chart1);         // Month Chart
+        lineChart =(LineChart) layout.findViewById(R.id.chart2);        // Weight chart
+        pieChart = (PieChart) layout.findViewById(R.id.chart3);         // Food
+        barChart2 = (BarChart) layout.findViewById(R.id.chart4);        // Week chart
+        //selectTime = (RadioGroup) layout.findViewById(R.id.TimeGroup);    // Time
         io = IODatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
         userID = mAuth.getCurrentUser().getUid();
-        //selectTime = (RadioGroup) layout.findViewById(R.id.TimeGroup);
-        //selectTime = (RadioGroup) layout.findViewById(R.id.TimeGroup);
 
-        barEntriesMonths = new ArrayList<>();
+        /*barEntriesMonths = new ArrayList<>();
         barEntriesMonths.add(new BarEntry(80f,0));
         barEntriesMonths.add(new BarEntry(200f,1));
         barEntriesMonths.add(new BarEntry(150f,2));
@@ -70,7 +68,7 @@ public class StatisticsFragment extends Fragment {
         barEntriesMonths.add(new BarEntry(45f,8));
         barEntriesMonths.add(new BarEntry(50f,9));
         barEntriesMonths.add(new BarEntry(150f,10));
-        barEntriesMonths.add(new BarEntry(45f,11));
+        barEntriesMonths.add(new BarEntry(45f,11));*/
 
         ArrayList<String> monthList = new ArrayList<>();
         monthList.add("Tam");
@@ -109,20 +107,21 @@ public class StatisticsFragment extends Fragment {
                     System.out.println(barEntriesMonths.size());
                 }
 
-                BarDataSet barDataSet = new BarDataSet(barEntriesMonths, "Co2 kulutus");
+                BarDataSet barDataSet = new BarDataSet(barEntriesMonths, "Co2 kulutus kg");
                 BarData theData = new BarData(monthList,barDataSet);
-                //BarData theData = new BarData(monthList,barDataSet);
+
                 barChart.setData(theData);
                 barChart.notifyDataSetChanged();
                 barChart.invalidate();
 
                 ArrayList pieEntry = new ArrayList();
                 pieEntry.add(new Entry(foodList.get(2).meat,0));
-                pieEntry.add(new Entry(foodList.get(2).restaurant,0));
-                pieEntry.add(new Entry(foodList.get(2).plant,0));
-                pieEntry.add(new Entry(foodList.get(2).dairy,0));
+                pieEntry.add(new Entry(foodList.get(2).restaurant,1));
+                pieEntry.add(new Entry(foodList.get(2).plant,2));
+                pieEntry.add(new Entry(foodList.get(2).dairy,3));
 
                 PieDataSet pieDataSet = new PieDataSet(pieEntry,"CO2 kulutus ruoka-aineittain");
+
                 ArrayList foodType  = new ArrayList();
                 foodType.add("Liha");
                 foodType.add("Ravintola");
@@ -133,11 +132,10 @@ public class StatisticsFragment extends Fragment {
                 pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
                 pieChart.notifyDataSetChanged();
                 pieChart.invalidate();
-
             }
         });
 
-        BarDataSet barDataSet = new BarDataSet(barEntriesMonths, "Co2 kulutus");
+        BarDataSet barDataSet = new BarDataSet(barEntriesMonths, "Co2 kulutus kg");
         BarData theData = new BarData(monthList,barDataSet);
         //BarData theData = new BarData(monthList,barDataSet);
         barChart.setData(theData);
@@ -147,36 +145,35 @@ public class StatisticsFragment extends Fragment {
         barChart.setDragEnabled(true);
         barChart.setScaleEnabled(true);
 
-        ArrayList<Entry> barEntries2 = new ArrayList<>();
-        barEntries2.add(new Entry(56, 1));
-        barEntries2.add(new Entry(57, 2));
-        barEntries2.add(new Entry(54, 3));
-        barEntries2.add(new Entry(59, 4));
-        barEntries2.add(new Entry(63, 5));
-        barEntries2.add(new Entry(60,6 ));
-        LineDataSet lineDataSet = new LineDataSet(barEntries2,"Paino");
+        ArrayList<Entry> weightEntries = new ArrayList<>();
+        weightEntries.add(new Entry(56, 0));
+        weightEntries.add(new Entry(57, 1));
+        weightEntries.add(new Entry(54, 2));
+        weightEntries.add(new Entry(59, 3));
+        weightEntries.add(new Entry(63, 4));
+        LineDataSet lineDataSet = new LineDataSet(weightEntries,"Paino");
         LineData lineData = new LineData(monthList, lineDataSet);
         lineChart.setData(lineData);
 
-        ArrayList pieEntry = new ArrayList();
-        pieEntry.add(new Entry(200,0));
-        pieEntry.add(new Entry(100,0));
-        pieEntry.add(new Entry(50,0));
-        pieEntry.add(new Entry(20,0));
-        pieEntry.add(new Entry(200,0));
+        /*ArrayList pieEntry = new ArrayList();
+                pieEntry.add(new Entry(50,0));
+                pieEntry.add(new Entry(150,1));
+                pieEntry.add(new Entry(75,2));
+                pieEntry.add(new Entry(20,3));
 
-        PieDataSet pieDataSet = new PieDataSet(pieEntry,"CO2 kulutus ruoka-aineittain");
+                PieDataSet pieDataSet = new PieDataSet(pieEntry,"CO2 kulutus ruoka-aineittain");
 
-        ArrayList foodType  = new ArrayList();
-        foodType.add("Liha");
-        foodType.add("Ravintola");
-        foodType.add("Kasvikset");
-        foodType.add("Maitotuotteet");
+                ArrayList foodType  = new ArrayList();
+                foodType.add("Liha");
+                foodType.add("Ravintola");
+                foodType.add("Kasvikset");
+                foodType.add("Maitotuotteet");
 
-        PieData pieData = new PieData(foodType,pieDataSet);
-        pieChart.setData(pieData);
-        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-
+                PieData pieData = new PieData(foodType,pieDataSet);
+                pieChart.setData(pieData);
+                pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+                //pieChart.notifyDataSetChanged();
+                //pieChart.invalidate();*/
         return layout;
     }
 
